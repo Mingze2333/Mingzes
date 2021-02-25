65b127e0e595adf7cc7ad44f2970d8888df0ef8c
Mingze2333 <3522339430@qq.co>
2021-02-25 15:39:36 +0800

    2021/2/25

--- 五子棋/screen.py
+++ 五子棋/screen.py
@@ -16,10 +16,16 @@ class Screen():  # 窗口类
         self.screen = pygame.display.set_mode((626, 626), 0)
         pygame.display.set_caption('五子棋')
 
-        # 加载图片和字体
+        # 加载图片
         self.bg = pygame.image.load('bg.gif').convert()
         self.piece_w = pygame.image.load('piece_w.gif').convert_alpha()
         self.piece_b = pygame.image.load('piece_b.gif').convert_alpha()
+
+        # 设置文字
+        font = pygame.font.Font('simkai.ttf', 100)
+        self.t1 = font.render(' 你赢了 ', True, (204, 51, 51))
+        self.t2 = font.render('电脑获胜', True, (204, 51, 51))
+        self.t3 = font.render('  平局  ', True, (204, 51, 51))
 
         # 创建棋子列表
         self.pieces_b = []  #　黑棋
@@ -52,23 +58,34 @@ class Screen():  # 窗口类
             if self.move:
                 # 检测1
                 result = self.game.result()
-                if result == 1:
-                    self.over(1)
+                if result != 0:
+                    break
                 # ai落子
                 ai_pos = self.game.ai_move()
                 self.pieces_w.append(self.get_point_pos[ai_pos])
                 # 检测2
                 result = self.game.result()
-                if result == 2:
-                    self.over(2)
-                # 检测3
-                result = self.game.result()
-                if result == 3:
-                    self.over(3)
+                if result != 0:
+                    break
                 # 允许玩家落子
                 self.move = False
 
             # 刷新屏幕
+            pygame.display.update()
+
+        while True:
+            # 监测事件
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+
+            if result == 1:
+                self.screen.blit(self.t1, (110, 260))
+            if result == 2:
+                self.screen.blit(self.t2, (110, 260))
+            if result == 3:
+                self.screen.blit(self.t3, (110, 260))
+
             pygame.display.update()
 
     def pos(self):  # 获取可落子点的坐标
@@ -94,34 +111,3 @@ class Screen():  # 窗口类
         self.move = self.game.move(pos)
         if self.move:
             self.pieces_b.append(point_pos)
-
-    def over(self, result):  # 结束窗口
-        # 设置窗口
-        over_screen = pygame.display.set_mode((400, 100), 0)
-        pygame.display.set_caption('游戏结束')
-        over_screen.fill((255, 255, 255))
-
-        # 设置字体
-        font = pygame.font.Font('simkai.ttf', 100)
-
-        # 设置文字
-        t1 = font.render(' 你赢了 ', True, (204, 51, 51))
-        t2 = font.render('电脑获胜', True, (204, 51, 51))
-        t3 = font.render('  平局  ', True, (204, 51, 51))
-
-        # 结束窗口循环
-        while True:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    exit()
-
-            # 绘制文字
-            if result == 1:
-                over_screen.blit(t1, (0, 0))
-            if result == 2:
-                over_screen.blit(t2, (0, 0))
-            if result == 3:
-                over_screen.blit(t3, (0, 0))
-
-            # 刷新屏幕
-            pygame.display.update()
